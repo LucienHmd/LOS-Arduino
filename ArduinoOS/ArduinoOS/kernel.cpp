2373e1c428ec3f9ff86f8b66c469eefc06b34daa
LukeLukeson <32346548+LukeLukeson@users.noreply.github.com>
2019-12-18 20:44:56 -0500

    Working Timer Interrupt

---
+++ ArduinoOS/ArduinoOS/kernel.cpp
@@ -0,0 +1,47 @@
+/*
+ * ArduinoOS.cpp
+ *
+ * Created: 12/16/2019 3:15:57 AM
+ * Author : Luke Lukeson
+ */ 
+
+
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000L // Specify oscillator frequency
+#include <stdio.h>
+#include <util/delay.h>
+
+
+#define DDRB = 0b10000000; // configure pin 7 of PORTB as output (digital pin 13 on the Arduino Mega2560)
+volatile unsigned long ticks = 0;
+volatile unsigned long x = 0;
+
+
+ISR(TIMER4_OVF_vect, ISR_BLOCK)
+{
+	ticks++;
+	
+	
+}
+
+unsigned long OSticks()
+{
+	return ticks;
+}
+
+
+void runOS()
+{
+	printf("w");
+	x = OSticks();
+	if (x > 10000) {
+		cli();
+		PORTB = 0b10000000; // set 7th bit to HIGH
+		_delay_ms(1000);
+		PORTB = 0b00000000; // set 7th bit to LOW
+		_delay_ms(1000);
+		x = 0;
+		sei();
+	}
+}
---
+++ ArduinoOS/ArduinoOS/main.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000L // Specify oscillator frequency
+
+
+int x = 0;
+
+
+
+int main(void)
+{
+	
+	sei();
+	TCCR4B |= (1 << CS40); // Sets timer 4 clock
+	TIMSK4 |=  (1 << TOIE4); // Enable Timer 4 Interrupts
+	DDRB |= (1 << PORTB7); // PORTB7 direction = out
+	
+	
+	while (1)
+	{
+		/*
+		// 1 pulse
+		PORTB |= (1 << PORTB7); // PORTB7 hi = LED L on
+		_delay_ms(10000); // 0.5 sec
+		PORTB &= ~(1 << PORTB7); // PORTB7 hi = LED L off
+		_delay_ms(10000); // 2 sec
+		
+		if(x == 1) {
+			PORTB |= (1 << PORTB7); // PORTB7 hi = LED L on
+			_delay_ms(10000);
+		} else {
+			PORTB |= ~(1 << PORTB7); // PORTB7 hi = LED L off
+			_delay_ms(10000);
+		}
+		*/
+	}
+	
+}
+
+ISR(TIMER4_OVF_vect, ISR_BLOCK)
+{
+	x = 1;
+	PORTB |= (1 << PORTB7); // set 7th bit to HIGH
+	_delay_ms(10000);
+	PORTB &= ~(1 << PORTB7); // set 7th bit to LOW
+	_delay_ms(10000);
+	
+}
